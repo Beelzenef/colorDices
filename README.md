2d2fa7f1ad81e57ed864fb80dcd9922ff21121e9
Elena G <elena.guzbla@gmail.com>
2017-11-20 14:08:14 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# colorDices
+
+Lanzando dados de colores
---
+++ app/src/main/java/com/example/colordices/LauncherActivity.java
@@ -0,0 +1,19 @@
+package com.example.colordices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+
+    public void onClick_CambiarDs(View v)
+    {
+
+    }
+}
